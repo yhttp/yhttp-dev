1eba50d7be9f523a1f48d3c8be1685ad4f03147e
pylover <vahid.mardani@gmail.com>
2024-08-18 20:46:40 +0330

    Migrate to yhttp >= 5

--- activate.sh
+++ activate.sh
@@ -1,3 +1,1 @@
-VENV=$(basename `dirname "$(readlink -f "$BASH_SOURCE")"`)
-VENVPATH=${HOME}/.virtualenvs/${VENV}
-source ${VENVPATH}/bin/activate
+make/activate.sh
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import re
-
 from os.path import join, dirname
-from setuptools import setup
+
+from setuptools import setup, find_namespace_packages
 
 
 # reading package version (same way the sqlalchemy does)
-with open(join(dirname(__file__), 'yhttp_devutils', '__init__.py')) as v_file:
+with open(join(dirname(__file__), 'yhttp/dev', '__init__.py')) as v_file:
     package_version = re.compile('.*__version__ = \'(.*?)\'', re.S).\
         match(v_file.read()).group(1)
 
@@ -16,9 +16,13 @@ dependencies = []
 setup(
     name='yhttp_devutils',
     version=package_version,
-    packages=['yhttp_devutils'],
     install_requires=dependencies,
     url='https://github.com/yhttp/yhttp-devutils',
     author='pylover',
     author_email='vahid.mardani@gmail.com',
+    packages=find_namespace_packages(
+        where='.',
+        include=['yhttp'],
+        exclude=['tests'],
+    )
 )
